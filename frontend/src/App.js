4f86b0d6c1dad3ba89a3a6c9e6503b506e605cc5
Kevin Rodriguez <87106659+Krcoded@users.noreply.github.com>
2021-09-25 14:59:51 -0400

    updated json

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
-import Coin from "./Coin";
+import Coin from "./views/Coin";
 
 function App() {
   const [coins, setCoins] = useState([]);
